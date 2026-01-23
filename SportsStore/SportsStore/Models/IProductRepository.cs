370520d6c5fd5e6fe432d63122564a04fec4e9ce
o3m2a1r <103195436+o3m2a1r@users.noreply.github.com>
2026-01-23 09:15:30 +0300

    Add a product repository interface

---
+++ SportsStore/SportsStore/Models/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SportsStore.Models
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> Products { get; }
+    }
+}
