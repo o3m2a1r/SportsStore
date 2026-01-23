177271ec4582071f3df3f8aa1b3a64dc8afea7ef
o3m2a1r <103195436+o3m2a1r@users.noreply.github.com>
2026-01-23 10:00:29 +0300

    Add the Product Controller

---
+++ SportsStore/SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+
+        public ActionResult List()
+        {
+            repository = new FakeProductRepository();
+            return View(repository.Products);
+        }
+
+    }
+}
